86633d2b0435e8ec41999f37f8012ed34e4c7905
yes-2875 <mirzafarrukh97@gmail.com>
2025-12-02 17:31:12 -0500

    Part 1 constructor finished

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // PART 1: Setting up Classes
 class ProductProperties {
-    
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
